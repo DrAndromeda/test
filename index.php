007894854e23f245e192fec969c10883972758df
DrAndromeda <kim_55@bigmir.net>
2015-03-05 15:31:03 +0200

    4

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
 	switch (substr($str_browser_language, 0,2))
 	{
+		VQPEIHG
+		QRG P
 		case 'de':
 			$str_language = 'de';
 			break;
 		case 'en':
-			$str_language = 'en';
+			 QE'ROGJP
+			 RE GPERJG
+			 .RG 2>
+			 RG $str_language = 'en';
 			break;
 		default:
 			$str_language = 'en';
