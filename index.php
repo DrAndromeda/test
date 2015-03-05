39c33b2d61875cc0ff6f990a05d3914cb618fb4e
Kim <kim_55@bigmir.net>
2015-03-05 15:56:02 +0200

    iuhiu
    oihoih

--- index.php
+++ index.php
@@ -5,19 +5,14 @@
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
 	switch (substr($str_browser_language, 0,2))
 	{
-		VQPEIHG
-		QRG P
+	
 		case 'de':
 			$str_language = 'de';
 			break;
-		case 'en':
-			 QE'ROGJP
-			 RE GPERJG
-			 .RG 2>
+	
 			 RG $str_language = 'en';
 			break;
 		default:
 			$str_language = 'en';
 	}
     
-	eofjbq[ef'pbjq]efjbq'efbjq[]
