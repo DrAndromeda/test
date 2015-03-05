cea49c4990fd31efd7932b308d6ab337ddd67b56
DrAndromeda <kim_55@bigmir.net>
2015-03-05 15:30:09 +0200

    TETST3

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!DOCPYPE HTML>
+<H1></H1>
 <?php
 	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
