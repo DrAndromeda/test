4e4e9fe3a5c96459b6fc6e541393866c6cc0e2f3
Kim <kim_55@bigmir.net>
2015-03-05 15:59:06 +0200

    jd
    d

--- index.php
+++ index.php
@@ -5,14 +5,5 @@
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
 	switch (substr($str_browser_language, 0,2))
 	{
-	
-		case 'de':
-			$str_language = 'de';
-			break;
-	
-			 RG $str_language = 'en';
-			break;
-		default:
-			$str_language = 'en';
 	}
     
